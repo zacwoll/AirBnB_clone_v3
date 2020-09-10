3340e4f48acd7a575d164ebcd6f2a807c8ad4438
zacwoll <zacwoll@gmail.com>
2020-09-10 15:55:41 +0000

    Made some of the init files executable and fixed import error caused by last commit

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,12 +4,13 @@ starts a Flask web application
 """
 from flask import Flask, Blueprint, jsonify
 from models import storage
-from api.v1.views import app_views
 from os import getenv
+from api.v1.views import app_views
 from flask_cors import CORS
 app = Flask(__name__)
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
 
 @app.teardown_appcontext
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,6 +3,7 @@
 Initialize app_views
 """
 from flask import Blueprint
+app_views = Blueprint('app_views', __name__, template_folder='templates')
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -10,4 +11,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-app_views = Blueprint('app_views', __name__, template_folder='templates')
